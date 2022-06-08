eef976d8e4fea27847c87cd64073cb1c4472167b
Семья Ким <dell200288@yandex.ru>
2022-06-09 00:51:55 +0300

    HW7

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
+        // Задание 1
+        String firsName = "Семён ";
+        String middleName = "Семёнович ";
+        String lastName = "Иванов ";
+        String fullName = lastName + firsName + middleName;
+        // Задание 2
+        fullName = fullName.toUpperCase();
+        // Задание 3
+        fullName = fullName.replace("ё", "е");
+        fullName = fullName.replace("Ё", "Е");
+        System.out.println("fullName = " + fullName);
 
     }
 }
